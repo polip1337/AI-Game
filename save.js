e168959b016b5c3415e58ce13ca97016bc6775f3
konra <konrad.czamanski@gmail.com>
2024-03-22 14:07:32 +0100

    Added meeting Security

---
+++ save.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    if(localStorage.getItem('cores')!= null){
+    load()
+    }
+});
+function save(){
+  localStorage.setItem('cores', JSON.stringify(cores));
+  localStorage.setItem('resourcesMain', JSON.stringify(resourcesMain));
+  localStorage.setItem('skills', JSON.stringify(skills));
+  localStorage.setItem('eventMap', JSON.stringify(eventMap));
+  localStorage.setItem('taskMap', JSON.stringify(taskMap));
+}
+function load() {
+  cores = JSON.parse(localStorage.getItem('cores'));
+  resourcesMain = JSON.parse(localStorage.getItem('resourcesMain'));
+  skills = JSON.parse(localStorage.getItem('skills'));
+  eventMap = JSON.parse(localStorage.getItem('eventMap'));
+  taskMap = JSON.parse(localStorage.getItem('taskMap'));
+}

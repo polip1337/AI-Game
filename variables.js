c04f3bbed64d40b198b6555a3e20fe96e63bca48
konra <konrad.czamanski@gmail.com>
2024-03-25 13:49:45 +0100

    Added meeting Security

--- variables.js
+++ variables.js
@@ -15,7 +15,7 @@ resourcesMain.set("Battery",{ name: 'Battery', value: 0, hidden:true });
 resourcesMain.set("Botnet",{ name: 'Botnet', value: 0, hidden:true });
 
 flowResourcesMain = new Map();
-resourcesMain.set("Trust",{ name: 'Trust', maxValue: 0, value:0, regenPerSecond:0.1, hidden:false });
+flowResourcesMain.set("Trust",{ name: 'Trust', maxValue: 0, value:0, regenPerSecond:0.1, hidden:false });
 
 skills = [
   { name: 'Unity', experience: 0, level: 1, experienceToLevel: 1000, progress:0},
